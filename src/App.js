a2bbc948b3442b3507fc46b6d5830e4141f8f85a
manojih <manoj@webknot.in>
2024-05-21 12:10:05 +0530

    Added mod 3 code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const cdDisplay = document.getElementById('countdown');
-  const startB = document.getElementById('start');
-  const stopB = document.getElementById('stop');
-  let cdTimer;
-  let totalSecRemaining;
+import "./App.css";
+import TicTacToe from "./components/TicTacToe";
 
-  startB.addEventListener('click', () => {
-      const hoursInput = parseInt(document.getElementById('hours').value) || 0;
-      const minutesInput = parseInt(document.getElementById('minutes').value) || 0;
-      const secondsInput = parseInt(document.getElementById('seconds').value) || 0;
+function App() {
+  return <TicTacToe />;
+}
 
-      totalSecRemaining = hoursInput * 3600 + minutesInput * 60 + secondsInput;
-      updateCountdownDisplay(totalSecRemaining);
-
-      if (cdTimer) clearInterval(cdTimer);
-
-      cdTimer = setInterval(() => {
-          if (totalSecRemaining > 0) {
-              totalSecRemaining--;
-              updateCountdownDisplay(totalSecRemaining);
-          } else {
-              clearInterval(cdTimer);
-              alert("Time's up!");
-          }
-      }, 1000);
-  });
-
-  stopB.addEventListener('click', () => {
-      if (cdTimer) clearInterval(cdTimer);
-  });
-
-  function updateCountdownDisplay(seconds) {
-      const hoursRemaining = Math.floor(seconds / 3600);
-      const minutesRemaining = Math.floor((seconds % 3600) / 60);
-      const secondsRemaining = seconds % 60;
-
-      cdDisplay.textContent = 
-          `${String(hoursRemaining).padStart(2, '0')}:
-           ${String(minutesRemaining).padStart(2, '0')}:
-           ${String(secondsRemaining).padStart(2, '0')}`;
-  }
-});
+export default App;
